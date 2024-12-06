eb6369c16d1733e44a046d3156ed1c843efe8e70
mulat <mulatram88@outlook.com>
2024-12-06 22:32:56 +0800

    Fix: Everything can run at 1.20.1 now!(Tested)

--- src/client/java/mypals/ml/mixin/client/WorldRenderMixin.java
+++ src/client/java/mypals/ml/mixin/client/WorldRenderMixin.java
@@ -2,9 +2,9 @@ package mypals.ml.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import mypals.ml.InfoRender;
-import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,10 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldRenderer.class)
 public class WorldRenderMixin {
 	@Inject(method = "render", at = @At(value = "INVOKE",target = "Lnet/minecraft/client/render/WorldRenderer;renderChunkDebugInfo(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/render/Camera;)V", ordinal = 0))
-	public void render(MatrixStack matrixStack,float tickDelta,
-								long limitTime, boolean renderBlockOutline,
-								Camera camera, GameRenderer gameRenderer,
-								LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci){
-		InfoRender.render(matrixStack,tickDelta);
+	public void render(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f projectionMatrix, CallbackInfo ci){
+		InfoRender.render(matrices,tickDelta);
 	}
 }
